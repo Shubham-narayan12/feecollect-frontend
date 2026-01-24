ea433402c23393477b57b63f4163a1e4b2f745e0
shubham <shubhamnarayan45@gmail.com>
2026-01-24 11:35:49 +0530

    intregate reciept api

--- src/api/receiptApi.js
+++ src/api/receiptApi.js
@@ -1,9 +1,26 @@
 import axiosInstance from "./axiosInstance.js";
 
 export const generateFeeReceipt = (formData) => {
-  return axiosInstance.post("/receipt/receipt-fee-collect", formData);
+  return axiosInstance.post("/receipt/recipt-collect-fee", formData);
 };
 
-export const downloadReceiptPdf = (fileName) => {
-  return axiosInstance.get(`/receipt/download/${fileName}`);
+export const downloadReceiptPdf = async (fileName) => {
+  const response = await axiosInstance.get(`/receipt/download/${fileName}`, {
+    responseType: "blob", // 🔥 THIS IS THE KEY
+  });
+
+  // Create blob URL
+  const blob = new Blob([response.data], { type: "application/pdf" });
+  const url = window.URL.createObjectURL(blob);
+
+  // Create hidden link & click
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", fileName);
+  document.body.appendChild(link);
+  link.click();
+
+  // Cleanup
+  link.remove();
+  window.URL.revokeObjectURL(url);
 };
